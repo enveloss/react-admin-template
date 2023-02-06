808cab039e9fd2c2ec7cf8d6f8d6c29aac055643
Artem <stroganov400@gmail.com>
2023-02-06 19:12:40 +0400

    removed message

--- api/modules.py
+++ api/modules.py
@@ -7,4 +7,3 @@ from utils.db_toolkit import db_toolkit as db
 # logger setuping
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
-logger.info('bot is started')
